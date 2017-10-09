ed73cb7a8ac5cc9cfa2fbd3de220f904911c7b80
Collins Agbonghama <collizo4sky@gmail.com>
2017-10-09 17:45:10 +0100

    Moved passing of lead data from core

--- src/AfterConversion.php
+++ src/AfterConversion.php
@@ -3,6 +3,7 @@
 namespace MailOptin\Libsodium;
 
 use MailOptin\Core\Admin\Customizer\CustomControls\WP_Customize_Ace_Editor_Control;
+use MailOptin\Core\Admin\Customizer\CustomControls\WP_Customize_Toggle_Control;
 use MailOptin\Core\Admin\Customizer\OptinForm\Customizer;
 use MailOptin\Core\Admin\Customizer\OptinForm\CustomizerControls;
 
@@ -21,6 +22,20 @@ class AfterConversion
      */
     public static function after_conversion_controls($controls, $wp_customize, $option_prefix, $customizerClassInstance)
     {
+        $controls['pass_lead_data_redirect_url'] = new WP_Customize_Toggle_Control(
+            $wp_customize,
+            $option_prefix . '[pass_lead_data_redirect_url]',
+            apply_filters('mo_optin_form_customizer_pass_lead_data_redirect_url_args', array(
+                    'label' => __('Pass Lead Data to Redirect URL', 'mailoptin'),
+                    'description' => __('Enabling this will add lead name and email address as a query parameter to the redirect URL you specified above.', 'mailoptin'),
+                    'section' => $customizerClassInstance->success_section_id,
+                    'settings' => $option_prefix . '[pass_lead_data_redirect_url]',
+                    'type' => 'light',
+                    'priority' => 30,
+                )
+            )
+        );
+
         $controls['success_js_script'] = new WP_Customize_Ace_Editor_Control(
             $wp_customize,
             $option_prefix . '[success_js_script]',
