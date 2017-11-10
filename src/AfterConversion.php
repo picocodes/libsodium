855a0e613c1a435bdf333eec2af57cd668fdaaa6
Collins Agbonghama <collizo4sky@gmail.com>
2017-11-10 19:41:37 +0100

    Added state after conversion feature.

--- src/AfterConversion.php
+++ src/AfterConversion.php
@@ -4,6 +4,7 @@ namespace MailOptin\Libsodium;
 
 use MailOptin\Core\Admin\Customizer\CustomControls\WP_Customize_Ace_Editor_Control;
 use MailOptin\Core\Admin\Customizer\CustomControls\WP_Customize_Toggle_Control;
+use MailOptin\Core\Admin\Customizer\OptinForm\AbstractCustomizer;
 use MailOptin\Core\Admin\Customizer\OptinForm\Customizer;
 use MailOptin\Core\Admin\Customizer\OptinForm\CustomizerControls;
 
@@ -11,7 +12,19 @@ class AfterConversion
 {
     public static function init()
     {
+        add_filter('mo_optin_form_customizer_success_settings', [__CLASS__, 'after_conversion_settings'], 10, 4);
         add_filter('mo_optin_form_customizer_success_controls', [__CLASS__, 'after_conversion_controls'], 10, 4);
+    }
+
+    public static function after_conversion_settings($settings)
+    {
+        $settings['state_after_conversion'] = array(
+            'default' => (new AbstractCustomizer())->customizer_defaults['state_after_conversion'],
+            'type' => 'option',
+            'transport' => 'postMessage',
+        );
+
+        return $settings;
     }
 
     /**
@@ -22,6 +35,24 @@ class AfterConversion
      */
     public static function after_conversion_controls($controls, $wp_customize, $option_prefix, $customizerClassInstance)
     {
+        $controls['state_after_conversion'] = apply_filters('mo_optin_form_customizer_state_after_conversion_args', array(
+                'type' => 'select',
+                'label' => __('State After Conversion', 'mailoptin'),
+                'section' => $customizerClassInstance->success_section_id,
+                'settings' => $option_prefix . '[state_after_conversion]',
+                'description' => sprintf(
+                    __('Choose state of optin form to users who are already subscribed. %sLearn more%s', 'mailoptin'),
+                    '<a target="_blank" href="#">', '</a>'
+                ),
+                'choices' => [
+                    'success_message_shown' => __('Success Message Shown', 'mailoptin'),
+                    'optin_form_hidden' => __('Optin Form Hidden', 'mailoptin'),
+                    'optin_form_shown' => __('Optin Form Shown', 'mailoptin'),
+                ],
+                'priority' => 25,
+            )
+        );
+
         $controls['pass_lead_data_redirect_url'] = new WP_Customize_Toggle_Control(
             $wp_customize,
             $option_prefix . '[pass_lead_data_redirect_url]',
