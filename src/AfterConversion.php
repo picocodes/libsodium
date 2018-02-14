0e9921b5fb1f29f84375fadbe65fbbb0f729afa3
Collins Agbonghama <collizo4sky@gmail.com>
2018-02-14 14:41:42 +0100

    Email notification after conversion WIP

--- src/AfterConversion.php
+++ src/AfterConversion.php
@@ -7,6 +7,8 @@ use MailOptin\Core\Admin\Customizer\CustomControls\WP_Customize_Toggle_Control;
 use MailOptin\Core\Admin\Customizer\OptinForm\AbstractCustomizer;
 use MailOptin\Core\Admin\Customizer\OptinForm\Customizer;
 use MailOptin\Core\Admin\Customizer\OptinForm\CustomizerControls;
+use MailOptin\Core\PluginSettings\Settings;
+use MailOptin\Core\Repositories\OptinCampaignsRepository;
 
 class AfterConversion
 {
@@ -14,6 +16,82 @@ class AfterConversion
     {
         add_filter('mo_optin_form_customizer_success_settings', [__CLASS__, 'after_conversion_settings'], 10, 4);
         add_filter('mo_optin_form_customizer_success_controls', [__CLASS__, 'after_conversion_controls'], 10, 4);
+
+        add_filter('mailoptin_settings_optin_campaign_settings_page', [__CLASS__, 'af_email_notification_settings']);
+        add_action('mailoptin_track_conversions', [__CLASS__, 'af_send_email_notification'], 10, 2);
+    }
+
+    private static function af_default_email_body()
+    {
+        return sprintf(
+            __("%s\n\n -- \n\nThis e-mail was sent by %s plugin on %s (%s)", 'mailoptin'), '[LEAD_DATA]', 'MailOptin', get_bloginfo('name'), site_url()
+        );
+    }
+
+    private static function af_default_subject()
+    {
+        return __('[OPTIN_CAMPAIGN] - New Lead', 'mailoptin');
+    }
+
+    public function af_send_email_notification($lead_data, $optin_campaign_id)
+    {
+        $emails = OptinCampaignsRepository::get_customizer_value($optin_campaign_id, 'email_notification');
+        $optin_campaign_name = OptinCampaignsRepository::get_optin_campaign_name($optin_campaign_id);
+
+        if (strpos($emails, ',') !== false) {
+            $emails = array_map('sanitize_email', explode(',', $emails));
+        }
+
+        $subject = Settings::instance()->afems_email_subject();
+        if (empty($subject)) {
+            $subject = self::af_default_subject();
+        }
+
+        $message_template = Settings::instance()->afems_message_contents();
+        if (empty($message_template)) {
+            $message_template = self::af_default_email_body();
+        }
+
+        $headers = [
+            'Content-Type: text/html; charset=UTF-8',
+        ];
+
+        $message = '';
+
+        if (is_array($lead_data)) {
+            unset($lead_data['optin_campaign_id']);
+            unset($lead_data['optin_campaign_type']);
+
+            foreach ($lead_data as $key => $value) {
+                $key = str_replace('_', ' ', $key);
+                $message .= '<p>' . ucfirst($key) . ': ' . $value . '</p>';
+            }
+        }
+
+        $subject = str_replace('[OPTIN_CAMPAIGN]', $optin_campaign_name, $subject);
+        $message_template = str_replace('[LEAD_DATA]', $message, $message_template);
+
+        wp_mail($emails, $subject, $message_template, $headers);
+    }
+
+    public static function af_email_notification_settings($settings)
+    {
+        $settings[] = [
+            'section_title' => __('After Conversion Email Notification Settings', 'mailoptin'),
+            'afems_email_subject' => [
+                'type' => 'text',
+                'label' => __('Email Subject', 'mailoptin'),
+                'value' => self::af_default_subject(),
+            ],
+            'afems_message_contents' => [
+                'type' => 'textarea',
+                'rows' => 10,
+                'value' => self::af_default_email_body(),
+                'label' => __('Email Message', 'mailoptin')
+            ]
+        ];
+
+        return $settings;
     }
 
     public static function after_conversion_settings($settings)
@@ -22,6 +100,12 @@ class AfterConversion
             'default' => (new AbstractCustomizer())->customizer_defaults['state_after_conversion'],
             'type' => 'option',
             'transport' => 'postMessage',
+        );
+
+        $settings['email_notification'] = array(
+            'default' => '',
+            'type' => 'option',
+            'transport' => 'postMessage'
         );
 
         return $settings;
@@ -66,6 +150,17 @@ class AfterConversion
                     'type' => 'light',
                     'priority' => 30,
                 )
+            )
+        );
+
+        $controls['email_notification'] = apply_filters('mo_optin_form_customizer_email_notification_args', array(
+                'type' => 'text',
+                'label' => __('Email Notification', 'mailoptin'),
+                'section' => $customizerClassInstance->success_section_id,
+                'settings' => $option_prefix . '[email_notification]',
+                'input_attrs' => ['placeholder' => __('Email Address', 'mailoptin')],
+                'description' => __('Enter email address to send notifications of new subscribers. You can add multiple email addresses separated by a comma. Leave blank to disable this feature.', 'mailoptin'),
+                'priority' => 35,
             )
         );
 
