500b54c9266639aa9bc0de9466dec1fc09120203
Collins Agbonghama <collizo4sky@gmail.com>
2018-05-10 10:40:57 +0100

    Hook admin notices to init

--- src/LibsodiumSettingsPage.php
+++ src/LibsodiumSettingsPage.php
@@ -26,8 +26,10 @@ class LibsodiumSettingsPage
             add_action('admin_init', array(__CLASS__, 'plugin_check_license'), 0);
         }
 
-        add_action('admin_notices', array(__CLASS__, 'license_not_active_notice'));
-        add_action('admin_notices', array(__CLASS__, 'license_expired_notice'));
+        add_action('init', function () {
+            add_action('admin_notices', array(__CLASS__, 'license_not_active_notice'));
+            add_action('admin_notices', array(__CLASS__, 'license_expired_notice'));
+        });
     }
 
     public static function license_key()
