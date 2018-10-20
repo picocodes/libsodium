ee46cc1494c64f3391c325855c38cf6fd9cb64d5
Collins Agbonghama <collizo4sky@gmail.com>
2018-10-20 08:44:14 +0100

    added rescript themes support for fontsize

--- src/PremiumTemplates/OptinForms/Sidebar/Rescript.php
+++ src/PremiumTemplates/OptinForms/Sidebar/Rescript.php
@@ -159,6 +159,71 @@ class Rescript extends AbstractOptinTheme
                     'value' => function () {
                         return '#919191';
                     }
+                ],
+
+
+
+                [
+                    'name'        => 'mo_optin_form_headline_font_size_desktop_default',
+                    'value'       => 24,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_headline_font_size_tablet_default',
+                    'value'       => 24,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_headline_font_size_mobile_default',
+                    'value'       => 20,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_description_font_size_desktop_default',
+                    'value'       => 16,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_description_font_size_tablet_default',
+                    'value'       => 16,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_description_font_size_mobile_default',
+                    'value'       => 14,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_note_font_size_desktop_default',
+                    'value'       => 14,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_note_font_size_tablet_default',
+                    'value'       => 14,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
+                ],
+
+                [
+                    'name'        => 'mo_optin_form_note_font_size_mobile_default',
+                    'value'       => 12,
+                    'optin_class' => 'Rescript',
+                    'optin_type'  => 'sidebar'
                 ]
             ]
         );
